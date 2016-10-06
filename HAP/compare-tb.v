f7db5e953da4e7a100725d257fd8d8368596ace9
kormin <abaotom14@gmail.com>
2016-10-06 23:36:49 +0800

    add test cases

--- HAP/compare-tb.v
+++ HAP/compare-tb.v
@@ -20,7 +20,85 @@ gtkwave compare.vpd
 	begin
 		$dumpfile("compare.vpd");
 		$dumpvars;
-			
+			// LT
+			opcode = 5'b01011;
+			R1 = 3'd3;
+			R2 = 3'd5;
+			#5
+			// LT
+			opcode = 5'b01011;
+			R1 = 3'd5;
+			R2 = 3'd3;
+			#5
+			// LT
+			opcode = 5'b01011;
+			R1 = 3'd3;
+			R2 = 3'd3;
+			#5
+			// GT
+			opcode = 5'b01100;
+			R1 = 3'd7;
+			R2 = 3'd3;
+			#5
+			// GT
+			opcode = 5'b01100;
+			R1 = 3'd3;
+			R2 = 3'd7;
+			#5
+			// GT
+			opcode = 5'b01100;
+			R1 = 3'd7;
+			R2 = 3'd7;
+			#5
+			// EQ
+			opcode = 5'b01101;
+			R1 = 3'd2;
+			R2 = 3'd2;
+			#5
+			// EQ
+			opcode = 5'b01101;
+			R1 = 3'd1;
+			R2 = 3'd6;
+			#5
+			// GTE
+			opcode = 5'b01110;
+			R1 = 3'd6;
+			R2 = 3'd1;
+			#5
+			// GTE
+			opcode = 5'b01110;
+			R1 = 3'd4;
+			R2 = 3'd4;
+			#5
+			// GTE
+			opcode = 5'b01110;
+			R1 = 3'd2;
+			R2 = 3'd4;
+			#5
+			// LTE
+			opcode = 5'b01111;
+			R1 = 3'd2;
+			R2 = 3'd6;
+			#5
+			// LTE
+			opcode = 5'b01111;
+			R1 = 3'd7;
+			R2 = 3'd7;
+			#5
+			// LTE
+			opcode = 5'b01111;
+			R1 = 3'd2;
+			R2 = 3'd7;
+			#5
+			// NE
+			opcode = 5'b10000;
+			R1 = 3'd2;
+			R2 = 3'd6;
+			#5
+			// NE
+			opcode = 5'b10000;
+			R1 = 3'd2;
+			R2 = 3'd2;
 			#5
 		$finish;
 	end
