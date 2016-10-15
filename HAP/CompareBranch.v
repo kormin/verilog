d44674def8ce1ddfeb8eb3fdbec9fd449c2e12b3
kormin <abaotom14@gmail.com>
2016-10-15 17:42:42 +0800

    add params

--- HAP/CompareBranch.v
+++ HAP/CompareBranch.v
@@ -12,4 +12,18 @@ module cmpBrnch (out, inp1, inp2, mode)
 	input [2:0] mode;
 	output [7:0] out;
 	
+	parameter Yes  = 7'b1;
+	parameter No   = 7'b0;
+	parameter LT   = 3'b000;
+	parameter GT   = 3'b001;
+	parameter EQ   = 3'b010;
+	parameter GTE  = 3'b011;
+	parameter LTE  = 3'b100;
+	parameter NE   = 3'b101;
+	parameter BNE  = 3'b111;
+	parameter BE   = 3'b110;
+	parameter BNER = 3'b111;
+	parameter BER  = 3'b110;
+	parameter J    = 3'b;
+	parameter JR   = 3'b;
 endmodule
