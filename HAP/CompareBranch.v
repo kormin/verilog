85d11db9943b0484c150e79ff57da4df5c1a9f9b
kormin <abaotom14@gmail.com>
2016-10-15 17:58:43 +0800

    add always

--- HAP/CompareBranch.v
+++ HAP/CompareBranch.v
@@ -7,13 +7,14 @@ iverilog -o cmp compare.v compare-tb.v
 vvp cmp
 gtkwave compare.vpd
  */
-module cmpBrnch (out, inp1, inp2, mode)
-	input [7:0] inp1, inp2;
+module cmpBrnch (out, R1, R2, mode)
+	input [7:0] R1, R2;
 	input [2:0] mode;
 	output [7:0] out;
 	
-	parameter Yes  = 7'b1;
-	parameter No   = 7'b0;
+	parameter Yes  = 1'd1;
+	parameter No   = 1'd0;
+	parameter AllNo = 8'd0;
 	parameter LT   = 3'b000;
 	parameter GT   = 3'b001;
 	parameter EQ   = 3'b010;
@@ -26,4 +27,26 @@ module cmpBrnch (out, inp1, inp2, mode)
 	parameter BER  = 3'b110;
 	parameter J    = 3'b;
 	parameter JR   = 3'b;
+	
+	reg [7:0] out;
+	always @ (*) begin
+		out [7:1] <= 6'd0;
+		case(mode)
+			// Compare
+			LT   : out [0] <= (R1  < R2) ? Yes : No;
+			GT   : out [0] <= (R1  > R2) ? Yes : No;
+			EQ   : out [0] <= (R1 == R2) ? Yes : No;
+			GTE  : out [0] <= (R1 >= R2) ? Yes : No;
+			LTE  : out [0] <= (R1 <= R2) ? Yes : No;
+			NE   : out [0] <= (R1 != R2) ? Yes : No;
+			// Branching
+			BNE  : out [0] <= (R1 != AllNo) ? Yes : No;
+			BE   : out [0] <= (R1 == AllNo) ? Yes : No;
+			BNER : out [0] <= (R1 != AllNo) ? Yes : No;
+			BER  : out [0] <= (R1 == AllNo) ? Yes : No;
+			// J    : out [0] <= Yes;
+			// JR   : out [0] <= R1;
+			// default: out [0] <= No;
+		endcase
+	end
 endmodule
