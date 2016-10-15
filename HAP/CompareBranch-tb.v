fc1a6666e21a0b79316538d184ea13e39cb354c2
kormin <abaotom14@gmail.com>
2016-10-15 18:06:16 +0800

    add tb

---
+++ HAP/CompareBranch-tb.v
@@ -0,0 +1,12 @@
+/*
+ * Author: 
+ * Project: Harvard Architecture Processor
+ * Module: Compare and Branching Instruction
+ * Script:
+iverilog -o cmpBrnch cmpBrnch.v cmpBrnch-tb.v
+vvp cmpBrnch
+gtkwave cmpBrnch.vpd
+ */
+module tb_cmpBrnch;
+
+endmodule
