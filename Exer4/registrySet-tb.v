c4431e2cdfb33edc990ed54338b6bab5976a2bd2
kormin <abaotom14@gmail.com>
2016-10-29 10:46:36 +0800

    add regset tb vals

--- Exer4/registrySet-tb.v
+++ Exer4/registrySet-tb.v
@@ -14,6 +14,62 @@ module tb_regset;
 	begin
 		$dumpfile("registrySet.vpd");
 		$dumpvars;
+			RW=0;
+			Address=3'b000;
+			Data_in=4'b0101;
+			#4
+			RW=0;
+			Address=3'b001;
+			Data_in=4'd15;
+			#4
+			RW=0;
+			Address=3'b010;
+			Data_in=4'd11;
+			#4
+			RW=0;
+			Address=3'b011;
+			Data_in=4'd8;
+			#4
+			RW=0;
+			Address=3'b100;
+			Data_in=4'd9;
+			#4
+			RW=0;
+			Address=3'b101;
+			Data_in=4'd4;
+			#4
+			RW=0;
+			Address=3'b110;
+			Data_in=4'd12;
+			#4
+			RW=0;
+			Address=3'b111;
+			Data_in=4'd7;
+			#4			
+			RW=1;
+			Address=3'b111;
+			#10
+			RW=1;
+			Address=3'b110;
+			#10
+			RW=1;
+			Address=3'b101;
+			#10
+			RW=1;
+			Address=3'b100;
+			#10
+			RW=1;
+			Address=3'b011;
+			#10
+			RW=0;
+			Address=3'b010;
+			#10
+			RW=1;
+			Address=3'b001;
+			#10
+			RW=1;
+			Address=3'b000;
+			#10
 			
 		$finish;
 	end
