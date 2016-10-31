8fef9700dea92c70691d6254633724b8c8fe723c
kormin <abaotom14@gmail.com>
2016-10-31 10:50:14 +0800

    add accum vals

--- Exer4/accumulator-tb.v
+++ Exer4/accumulator-tb.v
@@ -5,10 +5,36 @@ module tb_accum;
 	wire CBF;
 	wire [3:0] Y;
 	
+	accum UUT (Y, CBF, A, Clk, nReset);
+	
+	initial Clk = 0; 
+    always #2 Clk = ~Clk;
+	
 	initial
 	begin
 		$dumpfile("accumulator.vpd");
 		$dumpvars;
+			nReset=1;
+			A = 5'b00001;
+			#3
+			A = 5'b10001;
+			#3
+			A = 5'b11001;
+			#3
+			A = 5'b11001;
+			#3
+			A = 5'b11101;
+			nReset = 0;
+			#4
+			nReset=1;
+			A = 5'b11001;
+			#2
+			A = 5'b11111;
+			nReset = 0;
+			#4
+			A = 5'b11001;
+			#2
+			
 		$finish;
 	end
 endmodule
