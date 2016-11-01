f2fbf231439a034b885d553603559433fb970cc5
kormin <abaotom14@gmail.com>
2016-11-01 10:52:24 +0800

    add accum5 fxs

--- Exer4/accumulatorFive.v
+++ Exer4/accumulatorFive.v
@@ -11,4 +11,12 @@ module accum5 (Res,Of,A,B,Cin,Mode, Clk, nReset);
 	wire [3:0] temp_1, temp_2;
 	wire overflow, of1, of2;
 	
+	//alu4 AL0 (temp_1,overflow,A,B,Cin,Mode[2:0]);
+	//shifter SH0 (temp_2,A,Mode[2:0]);
+	//assign Res1 = (Mode[3] == 1) ? temp_2 : temp_1;
+	//assign of1 = (Mode[3] == 1) ? 1'bZ : overflow ;
+	alushifter ALU0 (Res1,of1,A,B,Cin,Mode);
+	accum Acc(Res, Of, {Res1,of1}, Clk, nReset);
+	//assign Res = Res2;
+	//assign Of = of2;
 endmodule
