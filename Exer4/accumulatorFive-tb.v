6f932d872ad902f04bd742849abe043d8818695e
kormin <abaotom14@gmail.com>
2016-11-02 10:58:03 +0800

    add accum5 vars and fx

--- Exer4/accumulatorFive-tb.v
+++ Exer4/accumulatorFive-tb.v
@@ -1,9 +1,19 @@
 module tb_accum5;
-	reg [4:0] A;
+	// reg [4:0] A;
 	reg Clk, nReset;
 	
-	wire CBF;
-	wire [3:0] Y;
+	// wire CBF;
+	// wire [3:0] Y;
+	
+	reg	[3:0] a, b, m;
+	reg cin;
+	wire [3:0] r;
+	wire of;
+	
+	accum5 UUT (r, of, a, b, cin, m, Clk, nReset);
+	
+	initial Clk = 0; 
+    always #2 Clk = ~Clk;
 	
 	initial
 	begin
